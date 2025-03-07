d301def28ae867ca6f736a8afa17b242fc09703d
NapasPolchai <polchai.napas@gmail.com>
2025-03-07 15:07:48 -0500

    Set up username, password

--- includes/connect.php
+++ includes/connect.php
@@ -2,9 +2,9 @@
  
  $connection = mysqli_connect('localhost','root','root','Portfolio'); 
 
- $dsn = "mysql:host=localhost;dbname=Portfolio;charset=utf8mb4";
+ $dsn = "mysql:host=localhost;dbname=nub53937_portfolio;charset=utf8mb4";
  try {
- $connection = new PDO($dsn, 'root', 'root');
+ $connection = new PDO($dsn, 'nub53937_napaspolchai', '28825PArN!');
  } catch (Exception $e) {
    error_log($e->getMessage());
    exit('unable to connect');
